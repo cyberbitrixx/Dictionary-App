81be6e84dcccc56080db0905b9b72ab2bd531738
cyberbitrixx <shchukinasofiya@gmail.com>
2025-02-20 16:47:54 -0800

    Initial Commit

---
+++ Dictionary/DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryApp.swift
+//  Dictionary
+//
+//  Created by sofia on 20.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
